9053ff3e3715bb888cbc4f324a11706f6eb49f68
tdelubac <timothee.delubac@gmail.com>
2018-02-20 12:07:32 +0100

    Initial commit

---
+++ mapyourdata.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import folium
+
+# Loading the geojson file with the shape of the french departments 
+departments = 'data/departements.geojson'
+
+# Loading the data with the number of counts per departments
+# We load the department column as string because it is a str
+# in the geojson file. 
+df_counts = pd.read_csv('data/counts_per_department.csv', dtype={'department':object})
+df_counts['department'] = df_counts['department'].astype(str)
+
+# Creating the initial map
+m = folium.Map(location=[46.5,2], zoom_start=5.49)
+
+# Creating the choropleth to add the coloured departments
+# on top of the map
+m.choropleth(
+    geo_data     = departments,
+    name         = 'French departments',
+    data         = df_counts,
+    columns      = ['department','counts'],
+    key_on       = 'feature.properties.code',
+    fill_color   = 'YlGn',
+    fill_opacity = 0.7,
+    line_opacity = 0.2,
+    legend_name  = 'Number of answers per department' 
+)
+folium.LayerControl().add_to(m)
+
+# Save to html and open it in your favorite browser
+m.save('departments.html')
